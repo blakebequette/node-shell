19c6faeca711516500923d0b2492c1635f12dfb6
beq <blakebequette@gmail.com>
2022-11-10 14:40:49 -0600

    add sort, head, tail

--- bash.js
+++ bash.js
@@ -3,6 +3,9 @@ const pwd = require("./pwd");
 const cat = require("./cat");
 const newDate = require("./date");
 const echo = require("./echo");
+const head = require('./head');
+const tail = require("./tail");
+const sort2 = require('./sort')
 
 const done = (output) => {
   process.stdout.write(output + "\n" + "prompt > ");
@@ -13,22 +16,31 @@ process.stdout.write("prompt > ");
 
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on("data", (data) => {
-  const cmd = data.toString().trim().split(" "); //remove the newline
+  const [cmd, ...args] = data.toString().trim().split(" "); //remove the newline
 
-  process.stdout.write("You typed: " + cmd[0] + "\n");
-  if (cmd[0] === "pwd") {
+  process.stdout.write("You typed: " + cmd + "\n");
+  if (cmd === "pwd") {
     pwd(done);
   }
-  if (cmd[0] === "ls") {
+  if (cmd === "ls") {
     ls(done);
   }
-  if (cmd[0] === "cat") {
-    cat(cmd[1], done);
+  if (cmd === "cat") {
+    cat(args[0], done);
   }
-  if (cmd[0] === "date") {
+  if (cmd === "date") {
     newDate(done);
   }
-  if (cmd[0] === "echo") {
+  if (cmd === "echo") {
     echo(done);
+  }
+  if (cmd === 'head') {
+    head(args[0], done)
+  }
+  if (cmd === 'tail') {
+    tail(args[0], done)
+  }
+  if (cmd === 'sort') {
+    sort2(args[0], done)
   }
 });
